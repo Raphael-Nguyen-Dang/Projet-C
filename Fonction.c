1cda65e70649bb63964444b7a62049b8c19ca4b5
Raphael-Nguyen-Dang <raphaelnguyendang@gmail.com>
2022-10-25 00:48:04 +0200

    ajout_du_fichier_fonction

---
+++ Fonction.c
@@ -0,0 +1,11 @@
+// Fichier contenant les divers fonctions utilisé dans notre code
+#include <stdio.h>
+
+
+int AffichageAllumette(int nombre){
+    //printf("Voici le nombre d'allumette restant : \n");
+    for (int x=0; x<nombre; x++){
+        printf(" |");
+    }
+}
+
